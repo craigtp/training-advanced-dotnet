f413d06aada9cfed8530fa4c3e514c61b6e4e97f
Craig Phillips <craig@craigtp.co.uk>
2021-06-11 20:00:01 +0100

    Implement the required code to allow the various "OverbookingProcessManagerTest" tests to pass.  Note this only implements the first part of the exercise, not the second part.

--- Scheduling/Application/OverbookingProcessManager.cs
+++ Scheduling/Application/OverbookingProcessManager.cs
@@ -15,17 +15,31 @@ namespace Scheduling.Domain.Application
         {
             When<SlotScheduled>(async (e, m) =>
             {
-                
+                await bookedSlotRepository.AddSlot(new BookedSlot
+                {
+                    Id = e.SlotId.ToString(),
+                    Month = e.SlotStartTime.Month,
+                    DayId = e.DayId
+                });
             });
 
             When<SlotBooked>(async (e, m) =>
             {
+                await bookedSlotRepository.MarkSlotAsBooked(e.SlotId.ToString(), e.PatientId);
 
+                var slot = await bookedSlotRepository.GetSlot(e.SlotId.ToString());
+                var count = await bookedSlotRepository.CountByPatientAndMonth(e.PatientId, slot.Month);
+
+                if (count > bookingLimitedPerPatient)
+                {
+                    await commandStore.Send(new CancelSlotBooking(e.DayId, e.SlotId, "overbooked"),
+                        new CommandMetadata(m.CorrelationId, new CausationId(idGenerator())));
+                }
             });
 
             When<SlotBookingCancelled>(async (e, m) =>
             {
-                
+                await bookedSlotRepository.MarkSlotAsAvailable(e.SlotId.ToString());
             });
         }
     }
